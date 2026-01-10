932baa8e3343a11da1a59e160d93f95d090c5309
lillecarl <git@lillecarl.com>
2026-01-10 02:10:08 +0000

    add script for creating initial secrets in job

--- flake.nix
+++ flake.nix
@@ -28,6 +28,39 @@
         in
         {
           inherit (pkgs) hello;
+          init-secrets = pkgs.writeShellApplication {
+            name = "push";
+            runtimeInputs = [ pkgs.coreutils pkgs.openssh  ];
+            text = # bash
+            ''
+              set -euo pipefail
+              mkdir -p /tmp/{ssh-key,nix-key}
+              if ! kubectl get secret ssh-key; then
+                # Create ssh secret
+                ssh-keygen -t ed25519 -C "" -N "" -f /tmp/ssh-key/id_ed25519
+                kubectl create secret generic ssh-key \
+                  --from-file=/tmp/ssh-key/id_ed25519.pub \
+                  --from-file=/tmp/ssh-key/id_ed25519
+
+                # Create pubkey configmap
+                cp /tmp/ssh-key/id_ed25519.pub /tmp/ssh-key/authorized_keys
+                echo "* $(cat /tmp/ssh-key/id_ed25519.pub)" > /tmp/ssh-key/ssh_known_hosts
+                kubectl create configmap ssh-dynauth \
+                  --from-file=/tmp/ssh-key/authorized_keys \
+                  --from-file=/tmp/ssh-key/ssh_known_hosts
+              fi
+              if ! kubectl get secret nix-key; then
+                # create nix binary cache key
+                nix-store --generate-binary-cache-key \
+                  nix-cache-1 \
+                  /tmp/nix-key/nix_ed25519 \
+                  /tmp/nix-key/nix_ed25519.pub
+                kubectl create secret generic ssh-key \
+                  --from-file=/tmp/nix-key/nix_ed25519.pub \
+                  --from-file=/tmp/nix-key/nix_ed25519
+              fi
+            '';
+          };
         }
       );
     };
