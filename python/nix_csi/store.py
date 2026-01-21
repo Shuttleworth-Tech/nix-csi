84c621b83bc5e96340871bf208bd3365a7f8c753
lillecarl <git@lillecarl.com>
2026-01-21 09:56:39 +0000

    simplify storePath extraction, only allow full storePaths
    because of how the volumemount experiment worked out and the need for
    hardlink farms with dereferenced symlinkx we can't use subPath mounting
    with storePaths.

--- python/nix_csi/store.py
+++ python/nix_csi/store.py
@@ -3,15 +3,14 @@ from collections.abc import Mapping, Sequence
 from pathlib import Path
 from typing import Any, Iterator
 
-# Nix base32 excludes: e, o, t, u
-STORE_PATH_RE = re.compile(r"/?nix/store/([0-9a-df-np-sv-z]{32}-[^\s/]+)")
+STORE_PATH_RE = re.compile(r"/nix/store/[a-z0-9]{32}-[^\s/]+")
 
 
 def extract_store_paths(value: Any) -> Iterator[Path]:
     match value:
         case str():
-            for match in STORE_PATH_RE.findall(value):
-                yield Path("/nix/store") / match
+            for m in STORE_PATH_RE.finditer(value):
+                yield Path(m.group())
         case Mapping():
             for v in value.values():
                 yield from extract_store_paths(v)
@@ -20,5 +19,5 @@ def extract_store_paths(value: Any) -> Iterator[Path]:
                 yield from extract_store_paths(item)
 
 
-def extract_store_name(value: Path | str) -> str:
-    return str(value).removeprefix("/nix/store/")
+def extract_store_name(path: Path | str) -> str:
+    return str(path).removeprefix("/nix/store/")
