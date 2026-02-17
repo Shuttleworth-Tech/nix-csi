641c959b5ed22d52392c8e0df945985f781822d8
lillecarl <git@lillecarl.com>
2026-02-17 21:26:31 +0000

    add users nixbld1-32, fixes #4
    hello, this is yolomcswaggins. i only test in ci

--- environments/modules/users.nix
+++ environments/modules/users.nix
@@ -2,9 +2,25 @@
 
 {
   pkgs,
+  lib,
   ...
 }:
 {
+  imports = [
+    {
+      # Add nixbld users
+      users.users = lib.pipe (lib.range 1 32) [
+        (map (i: {
+          name = "nixbld${toString i}";
+          value = {
+            uid = 30000 + i;
+            gid = 30000;
+          };
+        }))
+        lib.listToAttrs
+      ];
+    }
+  ];
   config = {
     users = {
       enable = true;
@@ -18,11 +34,16 @@
         comment = "Nix worker user";
       };
       groups.nix.gid = 1000;
-      groups.nixbld.gid = 30000;
-      users.sshd = {
-        uid = 993;
-        gid = 992;
-        comment = "SSH privilege separation user";
+      groups.nixbld = {
+        gid = 30000;
+        users = lib.map (i: "nixbld${toString i}") (lib.range 1 32);
+      };
+      users = {
+        sshd = {
+          uid = 993;
+          gid = 992;
+          comment = "SSH privilege separation user";
+        };
       };
       groups.sshd.gid = 992;
     };
